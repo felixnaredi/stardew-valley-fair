0d074fd34ee6615eaa5f2d3b12d11d911463cac4
felixnaredi <naredi@student.chalmers.se>
2022-01-17 00:43:01 +0100

    Animated bet label

--- src/components/BetLabel.vue
+++ src/components/BetLabel.vue
@@ -1,9 +1,54 @@
 <template>
-  <span class="display-4">Bet {{ $store.getters.bet }}</span>
+  <div
+    :class="{
+      'text-secondary': disabled,
+      'fst-italic': disabled,
+    }"
+  >
+    <span class="display-4">Bet </span>
+    <span id="bet-amount-label" class="display-4">{{ initialBet }}</span>
+  </div>
 </template>
 
 <script>
+import anime from "animejs/lib/anime.es.js";
+
 export default {
   name: "BetLabel",
+  data() {
+    return {
+      initialBet: null,
+      disabled: false,
+    };
+  },
+  created() {
+    this.initialBet = this.bet;
+    this.disabled = this.bet === 0;
+  },
+  computed: {
+    bet() {
+      return this.$store.getters.bet;
+    },
+  },
+  watch: {
+    bet(newValue, oldValue) {
+      const self = this;
+
+      anime({
+        targets: "#bet-amount-label",
+        innerHTML: [oldValue, newValue],
+        round: 1,
+        easing: "easeInOutExpo",
+        update(anim) {
+          if (anim.animations[0].currentValue > 0) {
+            self.disabled = false;
+          }
+          if (anim.animations[0].currentValue === 0) {
+            self.disabled = true;
+          }
+        },
+      });
+    },
+  },
 };
 </script>
