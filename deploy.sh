3cf31caf55d8ecbcce8659d76b6d56dd800ac6a3
felixnaredi <naredi@student.chalmers.se>
2022-01-16 19:08:31 +0100

    deploy pushes from default main branch

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ cd dist
 git init
 git add -A
 git commit -m "deploy"
-git push -f git@github.com:felixnaredi/stardew-valley-fair.git master:gh-pages
+git push -f git@github.com:felixnaredi/stardew-valley-fair.git main:gh-pages
 cd -
