3f414064150c0047f60f4a178950668ae4a4de36
felixnaredi <naredi@student.chalmers.se>
2022-01-17 03:34:17 +0100

    Animated token label

--- src/components/TokensLabel.vue
+++ src/components/TokensLabel.vue
@@ -2,9 +2,10 @@
   <main class="row">
     <span class="col-3">Tokens</span>
     <input
+      id="token-amount-label"
       class="col-3 border-0"
       type="text"
-      :value="$store.state.tokens"
+      :value="localTokenState"
       @change="customSetTokenAmount"
       aria-label="enter current token amount"
     />
@@ -12,10 +13,42 @@
 </template>
 
 <script>
+import anime from "animejs/lib/anime.es.js";
+
 export default {
   name: "TokensLabel",
+  data() {
+    return {
+      localTokenState: this.$store.state.tokens,
+      animate: true,
+    };
+  },
+  computed: {
+    tokens() {
+      return this.$store.state.tokens;
+    },
+  },
+  watch: {
+    tokens(newValue, oldValue) {
+      if (this.animate) {
+        anime({
+          targets: "#token-amount-label",
+          value: [oldValue, newValue],
+          round: 1,
+          easing: "easeInOutExpo",
+          complete() {
+            this.localTokenState = newValue;
+          },
+        });
+      } else {
+        this.localTokenState = newValue;
+        this.animate = true;
+      }
+    },
+  },
   methods: {
     customSetTokenAmount(event) {
+      this.animate = false;
       this.$store.commit("setTokens", event.target.value);
     },
   },
